823d0889ebf2c843544c14e7175f4c2365b7aa63
Vladislava Golubeva <slimki.kenni@yandex.ru>
2023-10-27 06:53:01 +0700

    feat(ui): реэкспорт компонентов

---
+++ client/src/components/index.ts
@@ -0,0 +1,5 @@
+import UserList from './user-list/UserList';
+import SearchInput from './search-input/SearchInput';
+import UserDetailModal from './user-detail-modal/UserDetailModal';
+
+export { UserList, SearchInput, UserDetailModal };
