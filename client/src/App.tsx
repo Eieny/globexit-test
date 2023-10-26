b997f2e99b3f836d6332a374871d8552b087e5cb
Vladislava Golubeva <slimki.kenni@yandex.ru>
2023-10-27 06:58:36 +0700

    feat(ui): основная страница

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import User from 'data/user';
+import { UserList, SearchInput, UserDetailModal } from 'components';
+import UserService from 'services/user.remote';
+import HttpClient from 'utils/http-client';
+import css from './App.module.css';
+
+const http = new HttpClient();
+const services = new UserService(http);
 
 function App() {
+  const [isModalOpen, toggleModal] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+  const [users, setUsers] = useState<User[]>([]);
+  const [selectedUser, selectUser] = useState<User | null>(null);
+
+  const getAllUsers = async () => {
+    const res = await services.all();
+    setUsers(res);
+  };
+
+  const getUserByName = async () => {
+    const res = await services.getByName(searchValue);
+    setUsers(res);
+  };
+
+  const handleClose = () => {
+    toggleModal(false);
+    selectUser(null);
+  };
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  useEffect(() => {
+    if (!selectedUser || isModalOpen) return;
+    toggleModal(true);
+  }, [selectedUser]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={css['App']}>
+      <main>
+        <SearchInput
+          value={searchValue}
+          onChange={setSearchValue}
+          onClick={getUserByName}
+        />
+        <UserList list={users} onClick={selectUser} />
+        <UserDetailModal
+          user={selectedUser}
+          isOpen={isModalOpen}
+          onClose={handleClose}
+        />
+      </main>
     </div>
   );
 }
